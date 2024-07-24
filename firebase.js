19593dffce14d14b02a6db31c980909f6a027c73
emilypondaven <emily.pondaven@gmail.com>
2024-07-24 17:16:12 +0100

    created firebase file

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAkrq6NGU0ocalA8QbN7y1aNiKJvUTBfZk",
+  authDomain: "notes-app-10839.firebaseapp.com",
+  projectId: "notes-app-10839",
+  storageBucket: "notes-app-10839.appspot.com",
+  messagingSenderId: "805112993966",
+  appId: "1:805112993966:web:1ed229f4722df3d4d1353f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+const notesCollection = collection(db, "notes")
